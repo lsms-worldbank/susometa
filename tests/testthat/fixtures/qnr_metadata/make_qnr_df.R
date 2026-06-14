67d54e05dde6237f528fc6905b3e9342db4e3c7f
Arthur Shaw <jashaw2002@gmail.com>
2026-06-13 23:23:16 -0400

    chore: remove questionnaire data frame fixture; categories only
    Since jq functions are fast and don't need that type of fixture.

--- tests/testthat/fixtures/qnr_metadata/make_qnr_df.R
+++ tests/testthat/fixtures/qnr_metadata/make_qnr_df.R
@@ -2,11 +2,6 @@
 # ingest data
 # ==============================================================================
 
-# questionnaire metadata
-qnr_df <- susometa::parse_questionnaire(
-  path = testthat::test_path("fixtures", "", "qnr_metadata", "document.json")
-)
-
 # reusable categories metadata
 reusable_categories_df <- susometa::parse_categories(
   dir = testthat::test_path("fixtures", "qnr_metadata", "Categories")
@@ -15,11 +10,6 @@ reusable_categories_df <- susometa::parse_categories(
 # ==============================================================================
 # write data to disk for persistent storage
 # ==============================================================================
-
-saveRDS(
-  object = qnr_df,
-  file = testthat::test_path("fixtures", "qnr_df.rds")
-)
 
 saveRDS(
   object = reusable_categories_df,
